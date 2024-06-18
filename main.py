d27330fa31120128522246b5f6c937d7e8291cae
kamil3242 <166690478+kamil3242@users.noreply.github.com>
2024-06-18 09:40:29 +0200

    wip: add loging option

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+def login():
+    print("logowanie")
+    username = input("Nazwa użytkownika: ")
+    password = input("Hasło: ")
+    if username == "Kamil" and password == "geoinformatyka":
+        print("zalogowano")
+        return True
+    else:
+        print("nie zalogowano")
+        return False
+
+
+if __name__ == "__main__":
+    print("witaj użytkowniku")
+    while True:
+        print("Menu:")
+        print("1. Login")

f426b230b7d66155dbd3132f1a860b794270be2a
kamil3242 <166690478+kamil3242@users.noreply.github.com>
2024-06-18 13:46:12 +0200

    wip: add function for showing and adding company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.data import users, employee, company
-from utils.crud import show_user, add_new_user, delete_user, edit_user
+from utils.crud import show_user, add_new_user, delete_user, edit_user, show_company, add_company
 
 def login():
     print("logowanie")
@@ -21,6 +21,11 @@ if __name__ == "__main__":
             print("2. dodaj nowego użytkownika")
             print("3. Usuń użytkownika")
             print("4. uaktualnij użytkownika")
+            print("5. Pokaż restauracje fast-food")
+            print("6. Dodaj nowe restauracje fast-food")
+            print("7. usuń restauracje fast-food")
+            print("8. uaktualnij restauracje fast-food")
+
 
 
             menu_option: str = input("Menu:")
@@ -36,3 +41,20 @@ if __name__ == "__main__":
             if menu_option == "4":
                 print("Kogo uaktualnic: ")
                 edit_user(users)
+            if menu_option == "5":
+                print("Restauracje fast-food: ")
+                show_company(company)
+            if menu_option == "6":
+                print("Dodaj restauracje fast-food: ")
+                add_company(company)
+            if menu_option == "8":
+                print("Usun restauracje fast-food: ")
+
+            if menu_option == "7":
+                print("Uaktualnij restauracje fast-food: ")
+
+
+
+
+
+

0f69325604d03b57b8d86db55e713b677372a98c
kamil3242 <166690478+kamil3242@users.noreply.github.com>
2024-06-18 09:25:24 +0200

    wip: add list in data

---
+++ models/data.py
@@ -0,0 +1,16 @@
+customer: list[dict] = [
+    {"name": "Kamil", "surname": "Kawa", "restauant": "McDonald"},
+    {"name": "Grzegorz", "surname": "Raz","restaurant": "KFC"}
+
+]
+
+employee: list[dict] = [
+    {"name": "Alan", "surname": "Waza", "restaurant": "McDonald"},
+    {"name": "Wojtek", "surname": "Zraz", "restaurant": "KFC"}
+]
+
+company: list[dict] = [
+    {"name": "McDonald", "customers": "4"},
+        {"name": "KFC", "customers": "2"}
+]
+

b012486e406113986dc83f244c6c2df118423006
kamil3242 <166690478+kamil3242@users.noreply.github.com>
2024-06-18 10:00:44 +0200

    wip: add def show customer

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-
-
-
-
+from models.data import customer
+from utils.crud import show_custmer
 
 def login():
     print("logowanie")
     username = input("Nazwa użytkownika: ")
     password = input("Hasło: ")
-    if username == "Kamil" and password == "geoinformatyka":
+    if username == "" and password == "":
         print("zalogowano")
         return True
     else:
         print("nie zalogowano")
         return False
-
+login()
 
 if __name__ == "__main__":
     print("witaj użytkowniku")
     while True:
         print("Menu:")
         break
-        print("1. Login")
--- utils/crud.py
+++ utils/crud.py
@@ -1,3 +1,11 @@
 def show_custmer(customer_list: list[dict]) -> None:
     for customer in customer_list:
         print(f"Imie: {customer['name']} Nazwisko: {customer['surname']}, Restauracja: {customer['restaurant']}")
+
+def add_new_customer(customer: list) -> None:
+    name = input("Imie: ")
+    surname = input("Nazwisko: ")
+    restaurant = input("Restauracja: ")
+    new_customer = {"name": name, "surname": surname, "restaurant": restaurant}
+    print(new_customer)
+    customer.append(new_customer)
